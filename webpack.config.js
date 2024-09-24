25e2df8cea91f78402344a7ab7b373de363b768b
fabricekx <fabricekx@gmail.com>
2024-09-24 10:59:39 +0200

    suppression du hash dans webpack.config .copyFiles

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ Encore
     .addEntry('app', './assets/app.js')
     .copyFiles({
         from: './assets/images',
-        to: 'images/[path][name].[hash:8].[ext]'
+        to: 'images/[path][name].[ext]'
+        // to: 'images/[path][name].[hash:8].[ext]'
     })
     .splitEntryChunks()
     .enableSingleRuntimeChunk()
